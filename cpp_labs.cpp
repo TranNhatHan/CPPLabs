1c10ef0e8e364f6f47befff5d7f8a3c1787c3c7e
Han <han2k3x2@gmail.com>
2025-01-24 10:16:50 +0000

    fix some files

--- cpp_labs.cpp
+++ cpp_labs.cpp
@@ -1,9 +1,8 @@
 #include "labs.hpp"
-#include <iostream>
 
 int main() {
-	//lab_one();
-	//lab_two();
-	lab_three();
-	return 0;
+  // lab_one();
+  // lab_two();
+  lab_three();
+  return 0;
 }
--- labs.h
+++
@@ -1,8 +0,0 @@
-#ifndef LABS_H
-#define LABS_H
-
-void lab_one();
-void lab_two();
-void rock_scissor_paper();
-
-#endif
